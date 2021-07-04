647088f38606e37ccd9d3208ac9d0fb6d6e06e81
Kumie Yoshida <iamkumie@KumienoMacBook-Pro.local>
2021-07-04 15:36:41 +0900

    third-fix

--- pages/register.vue
+++ pages/register.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="register">
-    <label>メールアドレス:<input v-model="email" type="email" required /></label>
+    <label
+      >メールアドレス： <input v-model="email" type="email" required
+    /></label>
     <br />
-    <label>パスワード:<input v-model="password" type="password" required /></label>
+    <label
+      >パスワード： <input v-model="password" type="password" required
+    /></label>
     <br />
     <button @click="register">新規登録</button>
     <br />
@@ -21,7 +25,7 @@ export default {
   },
   methods: {
     register() {
-      if(!this.email || !this.password) {
+      if (!this.email || !this.password) {
         alert('メールアドレスまたはパスワードが入力されていません。')
         return
       }
